bce9dfc7861118a844cc3090be186b1b3dcb069a
reach950 <reach950@gmail.com>
2018-11-09 09:15:12 +0800

    两两反转链表算法

--- linked_list.py
+++ linked_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""反转链表"""
+"""链表算法题"""
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -11,13 +11,34 @@
 
 
 class Solution:
-    def reverse_list(self, head):
+
+    # 反转链表
+    def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        curr_node = head
-        prev_node = None
-        while curr_node:
-            curr_node.next, prev_node, curr_node = prev_node, curr_node, curr_node.next
-        return prev_node
+        curr = head
+        prev = None
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        return prev
+
+    # 两两反转
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curr = head
+        prev = None
+        start = head.next
+        while curr and curr.next:
+            a = curr.next
+            if prev is not None:
+                prev.next = curr.next
+                prev = prev.next
+            else:
+                prev = curr
+            a.next, curr.next, curr = curr, a.next, curr.next.next
+        return start
