77a6ff255823cdd11fb9e1bb9374ef5c4e63278f
kejie <reach950@gmail.com>
2018-11-15 16:37:21 +0800

    添加验证括号是否合法的函数

--- leetcode.py
+++ leetcode.py
@@ -17,3 +17,24 @@ class Solution:
             if (target - x) in hash_map:
                 return [hash_map[target - x], i]
             hash_map[x] = i
+
+    # 验证括号是否合法
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        temp_stack = []
+        characters_dict = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        for i in s:
+            if i in characters_dict.values():
+                temp_stack.append(i)
+            elif not temp_stack or characters_dict[i] != temp_stack.pop():
+                return False
+        if temp_stack:
+            return False
+        return True
