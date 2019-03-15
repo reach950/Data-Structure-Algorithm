70933f185346b7e23b0b6ced39fdec1e03333727
kejie <reach950@gmail.com>
2019-03-15 18:58:11 +0800

    动态规划3

--- 数据结构与算法之美/dynamic_programming.py
+++ 数据结构与算法之美/dynamic_programming.py
@@ -89,6 +89,20 @@ class DynamicProgramming:
                     if min_nums[i] > 0 and i + j <= price:
                         min_nums[i + j] = 1 + min_nums[i]
 
+    # 求一个序列中的最长递增子序列长度
+    def find_sub_max_length(self, test_list):
+        size = len(test_list)
+        max_length_list = [1 for _ in range(size)]
+        for i in range(1, size):
+            for j in reversed(range(0, i)):
+                # 小于test_list[i]的最长子序列长度
+                max_i_length = 1
+                if test_list[i] > test_list[j]:
+                    max_i_length = 1 + max_length_list[j]
+                    break
+            max_length_list[i] = max(max_length_list[i-1], max_i_length)
+        return max_length_list[size - 1]
+
 
 if __name__ == '__main__':
     # items = [2, 2, 4, 6, 3]
@@ -97,4 +111,6 @@ if __name__ == '__main__':
     # print(DynamicProgramming().f_max_value(items, values, w))
     # yh_triangle = [[5], [7, 8], [2, 3, 4], [4, 9, 6, 1], [2, 7, 9, 4, 5]]
     # print(DynamicProgramming().find_min_sums(yh_triangle))
-    print(DynamicProgramming().find_min_coins([1, 3, 5], 10))
+    # print(DynamicProgramming().find_min_coins([1, 3, 5], 10))
+    test_list = [2, 9, 3, 6, 5, 1, 7]
+    print(DynamicProgramming().find_sub_max_length(test_list))
